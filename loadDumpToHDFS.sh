bce9fb18e01d5b66fc7f7877109e7a2c91392b44
Hadoop Distributed Computation Platform <hadoop@cpu17.nbl.cewit.stonybrook.edu>
2014-12-16 18:50:00 -0500

    Table name change in loadDumpToHDFS

--- loadDumpToHDFS.sh
+++ loadDumpToHDFS.sh
@@ -13,7 +13,7 @@ fi
 
 export HADOOP_HOME=/scratch/hadoop/hadoop-1.0.2
 export HBASE_HOME=/scratch/hadoop/hbase-0.94.22
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_45
 export LOCAL_DUMP_LOC=/scratch2/hadoop/Twitter/dumps
 
 if $HADOOP_HOME/bin/hadoop fs -test -d $dumploc; then
@@ -32,7 +32,7 @@ echo "Now putting $LOCAL_DUMP_LOC/$dumpdate.json to HDFS"
 $HADOOP_HOME/bin/hadoop fs -put $LOCAL_DUMP_LOC'/'$dumpdate.json $dumploc'/'$dumpdate'/'
 echo "Now counting number of tweets"
 lines=`wc -l $LOCAL_DUMP_LOC'/'$dumpdate.json | awk '{print $1;}'`
-echo "put 'tweetcounts','$dumpdate','tweetcount:ALL','$lines'" | $HBASE_HOME/bin/hbase shell
+echo "put 'andy_tweetcounts','$dumpdate','tweetcount:ALL','$lines'" | $HBASE_HOME/bin/hbase shell
 echo "Deleting $LOCAL_DUMP_LOC/$dumpdate.json from disk"
 rm $LOCAL_DUMP_LOC'/'$dumpdate.json
 echo "Done!"
